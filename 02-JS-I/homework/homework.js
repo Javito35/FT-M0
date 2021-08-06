857966cb6b30a7857897609cf427fd43e3367b5c
Franciscco Ataria <fjavierata@hotmail.com>
2021-08-05 21:14:02 -0300

    mis tareas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,9 +2,12 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
+const nuevaString = "Fran"
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
+const nuevoNum = "3"
+
 
 // Crea una variable booleana:
 const nuevoBool = null;
